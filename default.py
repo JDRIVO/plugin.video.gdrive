fb7c631e8c0d3159bfd85f58dc6d1915c4dd7394
durdle <ddurdle@gmail.com>
2014-03-07 21:39:17 -0500

    sync

--- default.py
+++ default.py
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from resources.lib import gdrive 
+from resources.lib import gdrive
 import sys
 import urllib
 import cgi
@@ -25,12 +25,12 @@ import re
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 
 
-#helper methods   
+#helper methods
 def log(msg, err=False):
     if err:
-        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGERROR)    
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGERROR)
     else:
-        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGDEBUG)    
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGDEBUG)
 
 def parse_query(query):
     queries = cgi.parse_qs(query)
@@ -40,18 +40,18 @@ def parse_query(query):
     q['mode'] = q.get('mode', 'main')
     return q
 
-def addVideo(url, infolabels, label, img='', fanart='', total_items=0, 
+def addVideo(url, infolabels, label, img='', fanart='', total_items=0,
                    cm=[], cm_replace=False):
     infolabels = decode_dict(infolabels)
     log('adding video: %s - %s' % (infolabels['title'].decode('utf-8','ignore'), url))
-    listitem = xbmcgui.ListItem(label, iconImage=img, 
+    listitem = xbmcgui.ListItem(label, iconImage=img,
                                 thumbnailImage=img)
     listitem.setInfo('video', infolabels)
     listitem.setProperty('IsPlayable', 'true')
     listitem.setProperty('fanart_image', fanart)
     if cm:
         listitem.addContextMenuItems(cm, cm_replace)
-    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem, 
+    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem,
                                 isFolder=False, totalItems=total_items)
 
 def addDirectory(url, title, img='', fanart='', total_items=0):
@@ -60,7 +60,7 @@ def addDirectory(url, title, img='', fanart='', total_items=0):
     if not fanart:
         fanart = addon.getAddonInfo('path') + '/fanart.jpg'
     listitem.setProperty('fanart_image', fanart)
-    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem, 
+    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem,
                                 isFolder=True, totalItems=total_items)
 
 #http://stackoverflow.com/questions/1208916/decoding-html-entities-with-python/1208931#1208931
@@ -89,6 +89,24 @@ plugin_queries = parse_query(sys.argv[2][1:])
 
 addon = xbmcaddon.Addon(id='plugin.video.gdrive')
 
+try:
+
+    remote_debugger = addon.getSetting('remote_debugger')
+    remote_debugger_host = addon.getSetting('remote_debugger_host')
+
+    # append pydev remote debugger
+    if remote_debugger == 'true':
+        # Make pydev debugger works for auto reload.
+        # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+        import pysrc.pydevd as pydevd
+        # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        pydevd.settrace(remote_debugger_host, stdoutToServer=True, stderrToServer=True)
+except ImportError:
+    sys.stderr.write("Error: " + "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+    sys.exit(1)
+except :
+    pass
+
 
 # retrieve settings
 username = addon.getSetting('username')
@@ -112,8 +130,8 @@ gdrive = gdrive.gdrive(username, password, auth_token, user_agent)
 # if we don't have an authorization token set for the plugin, set it with the recent login.
 #   auth_token will permit "quicker" login in future executions by reusing the existing login session (less HTTPS calls = quicker video transitions between clips)
 if auth_token == '' and save_auth_token == 'true':
-    addon.setSetting('auth_token', gdrive.writely)  
-    
+    addon.setSetting('auth_token', gdrive.writely)
+
 
 log('plugin google authorization: ' + gdrive.getHeadersEncoded())
 log('plugin url: ' + plugin_url)
@@ -139,8 +157,8 @@ if mode == 'main':
         addDirectory(videos[title],title)
     else:
       for title in sorted(videos.iterkeys()):
-        addVideo(videos[title]+'|'+gdrive.getHeadersEncoded(),  
-                             { 'title' : title , 'plot' : title }, title, 
+        addVideo(videos[title]+'|'+gdrive.getHeadersEncoded(),
+                             { 'title' : title , 'plot' : title }, title,
                              img='None')
 
 #play a URL that is passed in (presumely requires authorizated session)
@@ -150,7 +168,7 @@ elif mode == 'play':
 
     item = xbmcgui.ListItem(path=url+'|'+gdrive.getHeadersEncoded())
     log('play url: ' + url)
-    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item) 
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 #play a video given its exact-title
 elif mode == 'playvideo' or mode == 'playVideo':
@@ -165,7 +183,7 @@ elif mode == 'playvideo' or mode == 'playVideo':
 
     item = xbmcgui.ListItem(path=videoURL+'|'+gdrive.getHeadersEncoded())
     log('play url: ' + videoURL)
-    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item) 
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 #force memory-cache - play a video given its exact-title
 elif mode == 'memoryCacheVideo':
@@ -174,7 +192,7 @@ elif mode == 'memoryCacheVideo':
     videoURL = gdrive.getVideoLink(title)
     item = xbmcgui.ListItem(path=videoURL+'|'+gdrive.getHeadersEncoded())
     log('play url: ' + videoURL)
-    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item) 
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 
 #force stream - play a video given its exact-title
@@ -207,7 +225,7 @@ elif mode == 'streamVideo':
       videoURL = gdrive.getVideoLink(title, False, 2)
       item = xbmcgui.ListItem(path=videoURL+'|'+gdrive.getHeadersEncoded())
       log('play url: ' + videoURL)
-      xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item) 
+      xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 
 #clear the authorization token
@@ -217,7 +235,7 @@ elif mode == 'clearauth':
 
 # update the authorization token in the configuration file if we had to login for a new one during this execution run
 if auth_token != gdrive.writely and save_auth_token == 'true':
-    addon.setSetting('auth_token', gdrive.writely)  
-     
+    addon.setSetting('auth_token', gdrive.writely)
+
 xbmcplugin.endOfDirectory(plugin_handle)
 
