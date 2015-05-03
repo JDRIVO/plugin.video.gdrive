d883c4caaef4db3282f90775a3aa1a5dbc676937
ddurdle <ddurdle@gmail.com>
2015-05-02 20:28:42 -0400

    add settings module

---
+++ resources/lib/settings.py
@@ -0,0 +1,100 @@
+'''
+    Copyright (C) 2014 ddurdle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+
+#http://stackoverflow.com/questions/1208916/decoding-html-entities-with-python/1208931#1208931
+def _callback(matches):
+    id = matches.group(1)
+    try:
+        return unichr(int(id))
+    except:
+        return id
+
+def decode(data):
+    return re.sub("&#(\d+)(;|(?=\s))", _callback, data).strip()
+
+def getParameter(key,default=''):
+    try:
+        value = plugin_queries[key]
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            return value
+    except:
+        return default
+
+def getSetting(key,default=''):
+    try:
+        value = addon.getSetting(key)
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            return value
+    except:
+        return default
+
+def parse_query(query):
+    queries = cgi.parse_qs(query)
+    q = {}
+    for key, value in queries.items():
+        q[key] = value[0]
+    q['mode'] = q.get('mode', 'main')
+    return q
+
+
+def getParameter(key,default=''):
+    try:
+        value = plugin_queries[key]
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            return value
+    except:
+        return default
+
+def getSetting(key,default=''):
+    try:
+        value = addon.getSetting(key)
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            return value
+    except:
+        return default
+
+#
+#
+#
+class settings:
+    # Settings
+
+    ##
+    ##
+    def __init__(self):
+        self.test = 0
+
+
+
