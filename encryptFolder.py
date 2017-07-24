dc0bba217d3514424d0b955e3b03c114109b8f32
Dylan Durdle <ddurdle@gmail.com>
2017-07-23 21:07:04 -0400

    str(target) +'/'+ str(encFile)

--- encryptFolder.py
+++ encryptFolder.py
@@ -20,7 +20,14 @@ def encrypt_dir(source, target):
   current, dirs, files = os.walk(source).next()
 
   for file in files:
-    encrypt.encryptFile(source + '/' + file, target +'/'+encrypt.encryptString(file))
+    encFile = encrypt.encryptString(file)
+    encrypt.encryptFile(str(source) + '/' + str(file), str(target) +'/'+ str(encFile))
+    print str(source) + '/' + str(file)  + ' -> ' +  str(target) +'/'+ str(encFile)
+  for dir in dirs:
+    encDIR = encrypt.encryptString(dir)
+    os.mkdir(str(target) + '/' + str(encDIR))
+    encrypt_dir(str(source) + '/' + str(dir), str(target) +'/'+str(encDIR))
+    print str(source) + '/' + str(dir) + ' -> ' + str(target) + '/' + str(encDIR)
 
 #print encrypt.generateSalt()
 
