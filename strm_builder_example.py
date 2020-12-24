4e2dfdee4d35ffc3138594595343d2ebaf64db8f
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2020-12-25 02:45:15 +1100

    Create strm_builder_example.py

---
+++ strm_builder_example.py
@@ -0,0 +1,42 @@
+# To be ran by the script that uploads files to Google Drive
+
+import os
+import sys
+import subprocess
+import shlex
+import json
+
+videoPath = sys.argv[1]
+videoName = os.path.basename(videoPath)
+encryptedFilePath = sys.argv[2]
+strmPath = sys.argv[3]
+
+# videoPath = '/home/jack/strm/movies/best movie 2000.mkv'
+# encryptedFilePath = 'remote:/folder/RrtgFuvcxsWfKuFgGJ34VvcgfGFGWEWet'
+# strmPath = '/home/jack/strm/movies/best movie 2000.strm'
+
+cmd = 'ffprobe -v quiet -print_format json -show_format -show_streams'
+args = shlex.split(cmd)
+args.append(videoPath)
+ffprobeOutput = subprocess.check_output(args).decode('utf-8')
+ffprobeOutput = json.loads(ffprobeOutput)
+
+if 'codec_name' in ffprobeOutput['streams'][0]:
+	videoCodec = ffprobeOutput['streams'][0]['codec_name']
+else:
+	videoCodec = 'unknown'
+
+videoWidth = str(ffprobeOutput['streams'][0]['width'])
+videoHeight = str(ffprobeOutput['streams'][0]['height'])
+videoDuration = str(ffprobeOutput['format']['duration'])
+aspectRatio = str(float(videoWidth) / float(videoHeight) )
+audioCodec = ffprobeOutput['streams'][1]['codec_name']
+audioChannels = str(ffprobeOutput['streams'][1]['channels'])
+
+cmd = 'rclone lsf --format i'
+args = shlex.split(cmd)
+args.append(encryptedFilePath)
+fileID = subprocess.check_output(args).strip()
+
+with open(strmPath, "w+") as strm:
+	strm.write("plugin://plugin.video.gdrive/?mode=video&encfs=True&title=%s&video_codec=%s&video_width=%s&video_height=%s&video_duration=%s&aspect_ratio=%s&audio_codec=%s&audio_channels=%s&filename=%s" % ( videoName, videoCodec, videoWidth, videoHeight, videoDuration, aspectRatio, audioCodec, audioChannels, fileID) )
