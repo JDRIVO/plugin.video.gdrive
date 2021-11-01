c50a35b2d0124df4e146a4f6fc7af95bae5e636e
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-11-01 12:15:08 +1100

    Update strm_builder_example.py

--- strm_builder_example.py
+++ strm_builder_example.py
@@ -1,10 +1,9 @@
 # To be ran by the script that uploads files to Google Drive. Sync strms with resilio, syncthing etc. Install watchdog on Kodi.
 
-import os
 import sys
-import subprocess
-import shlex
 import json
+import shlex
+import subprocess
 
 videoPath = sys.argv[1]
 encryptedFilePath = sys.argv[2]
@@ -20,17 +19,20 @@ args.append(videoPath)
 ffprobeOutput = subprocess.check_output(args).decode('utf-8')
 ffprobeOutput = json.loads(ffprobeOutput)
 
-if 'codec_name' in ffprobeOutput['streams'][0]:
-	videoCodec = ffprobeOutput['streams'][0]['codec_name']
-else:
-	videoCodec = 'unknown'
-
-videoWidth = str(ffprobeOutput['streams'][0]['width'])
-videoHeight = str(ffprobeOutput['streams'][0]['height'])
 videoDuration = str(ffprobeOutput['format']['duration'])
-aspectRatio = str(float(videoWidth) / float(videoHeight) )
-audioCodec = ffprobeOutput['streams'][1]['codec_name']
-audioChannels = str(ffprobeOutput['streams'][1]['channels'])
+
+for dic in ffprobeOutput['streams']:
+
+	if dic['codec_type'] == 'video':
+		videoCodec = dic['codec_name']
+		videoWidth = dic['width']
+		videoHeight = dic['height']
+		aspectRatio = str(videoWidth / videoHeight)
+		videoWidth = str(videoWidth)
+		videoHeight = str(videoHeight)
+	elif dic['codec_type'] == 'audio':
+		audioCodec = dic['codec_name']
+		audioChannels = dic['channels']
 
 cmd = 'rclone lsf --format i'
 args = shlex.split(cmd)
@@ -40,6 +42,4 @@ fileID = subprocess.check_output(args).strip().decode('utf-8')
 with open(strmPath, 'w+') as strm:
 	# Every paramater is optional besides the filename (Google Drive File ID) - essential strm format is:
 	# plugin://plugin.video.gdrive/?mode=video&encfs=True&filename=7ctPNMUl4m8B4KBwY
-
-	strm.write('''plugin://plugin.video.gdrive/?mode=video&encfs=True&video_codec=%s&video_width=%s&video_height=%s&video_duration=%s&aspect_ratio=%s
-		   &audio_codec=%s&audio_channels=%s&filename=%s''' % (videoCodec, videoWidth, videoHeight, videoDuration, aspectRatio, audioCodec, audioChannels, fileID) )
+	strm.write("plugin://plugin.video.gdrive/?mode=video&encfs=True&video_codec=%s&video_width=%s&video_height=%s&video_duration=%s&aspect_ratio=%s&audio_codec=%s&audio_channels=%s&filename=%s" % (videoCodec, videoWidth, videoHeight, videoDuration, aspectRatio, audioCodec, audioChannels, fileID))
