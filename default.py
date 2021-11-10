3790f4e81be4bc33e1e8642aff5c51ab4ac81a5e
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-11-11 10:23:15 +1100

    Update default.py

--- default.py
+++ default.py
@@ -18,11 +18,11 @@
 
 import xbmc
 
-# container = xbmc.getInfoLabel('System.CurrentControlID')
-# dbID = xbmc.getInfoLabel('Container(%s).ListItem.DBID' % container)
-# dbType = xbmc.getInfoLabel('Container(%s).ListItem.DBTYPE' % container)
-# dbType = xbmc.getInfoLabel('Container.ListItem.DBID')
-# dbID = xbmc.getInfoLabel('ListItem.FolderPath').split('?')[0].rstrip('/').split('/')[-1]
+# container = xbmc.getInfoLabel("System.CurrentControlID")
+# dbID = xbmc.getInfoLabel("Container(%s).ListItem.DBID" % container)
+# dbType = xbmc.getInfoLabel("Container(%s).ListItem.DBTYPE" % container)
+# dbType = xbmc.getInfoLabel("Container.ListItem.DBID")
+# dbID = xbmc.getInfoLabel("ListItem.FolderPath").split("?")[0].rstrip("/").split("/")[-1]
 
 dbID = xbmc.getInfoLabel("ListItem.DBID")
 dbType = xbmc.getInfoLabel("ListItem.DBTYPE")
