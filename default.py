1d68cda2f2d941d719681be2927ecd0ca8a2a031
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-11-11 10:22:27 +1100

    Update default.py

--- default.py
+++ default.py
@@ -1,20 +1,20 @@
-'''
-    CloudService XBMC Plugin
-    Copyright (C) 2013-2014 ddurdle
+"""
+	CloudService XBMC Plugin
+	Copyright (C) 2013-2014 ddurdle
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import xbmc
 
@@ -24,9 +24,9 @@ import xbmc
 # dbType = xbmc.getInfoLabel('Container.ListItem.DBID')
 # dbID = xbmc.getInfoLabel('ListItem.FolderPath').split('?')[0].rstrip('/').split('/')[-1]
 
-dbID = xbmc.getInfoLabel('ListItem.DBID')
-dbType = xbmc.getInfoLabel('ListItem.DBTYPE')
-filePath = xbmc.getInfoLabel('ListItem.FolderPath')
+dbID = xbmc.getInfoLabel("ListItem.DBID")
+dbType = xbmc.getInfoLabel("ListItem.DBTYPE")
+filePath = xbmc.getInfoLabel("ListItem.FolderPath")
 
 from resources.lib import engine
 
