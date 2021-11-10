416eb0495c119683f8c1d4ae1ff7d0c2c251498e
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-11-11 10:26:07 +1100

    Cleanup

--- default.py
+++ default.py
@@ -1,6 +1,5 @@
 """
-	CloudService XBMC Plugin
-	Copyright (C) 2013-2014 ddurdle
+	Copyright (C) 2014-2016 ddurdle
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -14,6 +13,8 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 
 import xbmc
