368caf726b54542b611c8187fea84ffa15290de5
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-12-13 18:08:04 +1100

    Update server.py

--- server.py
+++ server.py
@@ -11,13 +11,6 @@ def run():
 	port = settings.getSettingInt("server_port", 8011)
 	server = streamer.MyHTTPServer(("", port), streamer.MyStreamer)
 
-	try:
-		pluginHandle = int(sys.argv[1])
-		pluginQueries = settings.parse_query(sys.argv[2][1:])
-	except:
-		pluginHandle = None
-		pluginQueries = None
-
 	server.setDetails(pluginName, settings)
 	Thread(target=server.run, daemon=True).start()
 	monitor = xbmc.Monitor()
