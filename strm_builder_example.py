10e4f27d29f33fc860254c4ebb918271845949ff
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-12-22 20:21:55 +1100

    Update strm_builder_example.py

--- strm_builder_example.py
+++ strm_builder_example.py
@@ -48,6 +48,9 @@ for dic in ffprobeOutput["streams"]:
 
 		if codecTag in ("dva1", "dvav", "dvh1", "dvhe"):
 			mediaInfo["hdr"] = "dolbyvision"
+			codecs = {"dva1": "h264", "dvav": "h264", "dvh1": "hevc", "dvhe": "hevc"}
+			mediaInfo["video_codec"] = codecs[codecTag]
+
 		elif colourTransfer in ("smpte2084", "smpte2086"):
 			mediaInfo["hdr"] = "hdr10"
 		elif colourTransfer == "arib-std-b67":
