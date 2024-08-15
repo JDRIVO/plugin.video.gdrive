a7befdcc6eaab7bd168719e768a2721a3c6d384a
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2024-08-15 21:11:36 +1000

    Update syncer.py

--- resources/lib/sync/syncer.py
+++ resources/lib/sync/syncer.py
@@ -283,7 +283,7 @@ class Syncer:
 			else:
 				cachedFilePath = os.path.join(syncRootPath, cachedFile["local_path"])
 
-			if not cachedFile["original_name"] or not cachedFile["original_folder"] or not os.path.exists(cachedFilePath) or (cachedFile["remote_name"] == filename and cachedDirPath == dirPath):
+			if (cachedFile["remote_name"] == filename and cachedDirPath == dirPath) or not cachedFile["original_name"] or not cachedFile["original_folder"] or not os.path.exists(cachedFilePath):
 				# new filename needs to be processed or file not existent or file contents modified > redownload file
 				self.fileOperations.deleteFile(syncRootPath, filePath=cachedFilePath)
 				self.cache.deleteFile(fileID)
