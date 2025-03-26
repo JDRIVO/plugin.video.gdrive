87dfc6af59300160bfc975e0b15b358f16e16ef2
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2025-03-26 22:17:40 +1100

    Cleanup

--- resources/lib/network/network_helpers.py
+++ resources/lib/network/network_helpers.py
@@ -4,11 +4,14 @@ import urllib.parse
 def addQueryString(url, params):
 	return f"{url}?{urllib.parse.urlencode(params)}"
 
+
 def mergePaths(baseURL, paths):
 	return f"{baseURL}/{paths}" if isinstance(paths, str) else f"{baseURL}/{'/'.join(paths)}"
 
+
 def parseQuery(query):
 	return dict(urllib.parse.parse_qsl(query))
+
 
 def parseURL(urlString):
 	url = urllib.parse.urlparse(urlString)
@@ -18,6 +21,7 @@ def parseURL(urlString):
 		query = parseQuery(query)
 
 	return {"path": url.path, "query": query}
+
 
 def quote(string):
 	return urllib.parse.quote(string)
