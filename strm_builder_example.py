e04efb8aec6997041d8423422693d2d04d783d08
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2025-04-20 20:32:42 +1000

    Update strm_builder_example.py

--- strm_builder_example.py
+++ strm_builder_example.py
@@ -10,12 +10,14 @@ import subprocess
 videoPath = sys.argv[1]
 remoteVideoPath = sys.argv[2]
 strmPath = sys.argv[3]
-driveID = sys.argv[4] # Optional - if not included you will need to manually set the playback account in settings
+driveID = sys.argv[4] # Optional - if not specified you will need to set a default playback account in the addon settings
+encrypted = sys.argv[5]
 
 # videoPath = "/home/jack/movies/best movie 2000.mkv"
 # remoteVideoPath = "remote:/folder/RrtgFuvcxsWfKuFgGJ34VvcgfGFGWEWet"
 # strmPath = "/home/jack/strm/movies/best movie 2000.strm"
 # driveID = "0AGHbAfUtSfVATg4F5B"
+# encrypted = False
 
 cmd = "ffprobe -v quiet -print_format json -show_format -show_streams"
 args = shlex.split(cmd)
@@ -23,87 +25,91 @@ args.append(videoPath)
 ffprobeOutput = subprocess.check_output(args).decode("utf-8")
 ffprobeOutput = json.loads(ffprobeOutput)
 
-strmInfo = {}
-strmInfo["video_duration"] = ffprobeOutput["format"]["duration"]
+strmData = {}
+strmData["video_duration"] = ffprobeOutput["format"]["duration"]
 video = audio = False
 
-for dic in ffprobeOutput["streams"]:
-	codecType = dic.get("codec_type")
+for data in ffprobeOutput["streams"]:
+	codecType = data.get("codec_type")
 
 	if codecType == "video":
 
-		if strmInfo.get("hdr") == "dolbyvision":
+		if strmData.get("hdr") == "dolbyvision":
 			continue
 
-		codecTag = dic.get("codec_tag_string")
-		colourTransfer = dic.get("color_transfer")
-		sideData = dic.get("side_data_list")
+		codecTag = data.get("codec_tag_string")
+		colourTransfer = data.get("color_transfer")
+		sideData = data.get("side_data_list")
 
 		if codecTag in ("dva1", "dvav", "dvh1", "dvhe"):
-			strmInfo["hdr"] = "dolbyvision"
+			strmData["hdr"] = "dolbyvision"
 			codecs = {"dva1": "h264", "dvav": "h264", "dvh1": "hevc", "dvhe": "hevc"}
-			strmInfo["video_codec"] = codecs[codecTag]
+			strmData["video_codec"] = codecs[codecTag]
 		elif sideData and "dv_profile" in str(sideData):
-			strmInfo["hdr"] = "dolbyvision"
+			strmData["hdr"] = "dolbyvision"
 		elif colourTransfer in ("smpte2084", "smpte2086", "smpte2094"):
-			strmInfo["hdr"] = "hdr10"
+			strmData["hdr"] = "hdr10"
 		elif colourTransfer == "arib-std-b67":
-			strmInfo["hdr"] = "hlg"
+			strmData["hdr"] = "hlg"
 
 		if video:
 			continue
 
 		video = True
-		videoCodec = dic.get("codec_name")
-		videoWidth = dic.get("width")
-		videoHeight = dic.get("height")
+		videoCodec = data.get("codec_name")
+		videoWidth = data.get("width")
+		videoHeight = data.get("height")
 
-		if videoCodec and not strmInfo.get("video_codec"):
-			strmInfo["video_codec"] = videoCodec
+		if videoCodec and not strmData.get("video_codec"):
+			strmData["video_codec"] = videoCodec
 
 		if videoWidth:
-			strmInfo["video_width"] = videoWidth
+			strmData["video_width"] = videoWidth
 
 		if videoHeight:
-			strmInfo["video_height"] = videoHeight
+			strmData["video_height"] = videoHeight
 
 		if videoWidth and videoHeight:
-			strmInfo["aspect_ratio"] = videoWidth / videoHeight
+			strmData["aspect_ratio"] = videoWidth / videoHeight
 
 	elif codecType == "audio" and not audio:
 		audio = True
-		audioCodec = dic.get("codec_name")
-		audioChannels = dic.get("channels")
+		audioCodec = data.get("codec_name")
+		audioChannels = data.get("channels")
 
 		if audioCodec:
-			strmInfo["audio_codec"] = audioCodec
+			strmData["audio_codec"] = audioCodec
 
 		if audioChannels:
-			strmInfo["audio_channels"] = audioChannels
+			strmData["audio_channels"] = audioChannels
 
 filename = os.path.basename(videoPath)
 
 if re.search("[-_. ](dv|dovi|dolby[-_. ]*vision)[-_. ]", filename, re.IGNORECASE):
-	strmInfo["hdr"] = "dolbyvision"
-elif not strmInfo.get("hdr") and re.search("hdr10", filename, re.IGNORECASE):
-	strmInfo["hdr"] = "hdr10"
+	strmData["hdr"] = "dolbyvision"
+elif not strmData.get("hdr") and re.search("hdr10", filename, re.IGNORECASE):
+	strmData["hdr"] = "hdr10"
+
+if encrypted:
+	strmData["encrypted"] = True
+
+if driveID:
+	strmData["drive_id"] = driveID
 
 cmd = "rclone lsf --format i"
 args = shlex.split(cmd)
 args.append(remoteVideoPath)
 fileID = subprocess.check_output(args).strip().decode("utf-8")
-strmInfo["file_id"] = fileID
-
-if driveID:
-	strmInfo["drive_id"] = driveID
+strmData["file_id"] = fileID
 
 with open(strmPath, "w+") as strm:
 
 	# Essential strm format for unencrypted videos:
-	# plugin://plugin.video.gdrive/?mode=video&encrypted=False&file_id=7ctPNMUl4m8B4KBwY
+	# plugin://plugin.video.gdrive/?mode=video&file_id=7ctPNMUl4m8B4KBwY
 
-	# Essential strm format for gDrive encrypted videos:
+	# Essential strm format for encrypted videos:
 	# plugin://plugin.video.gdrive/?mode=video&encrypted=True&file_id=7ctPNMUl4m8B4KBwY
 
-	url = "plugin://plugin.video.gdrive/?mode=video&encrypted=True" + "".join(["&{}={}".format(k, v) for k, v in strmInfo.items()])
+	url = "plugin://plugin.video.gdrive/?mode=video"
+	url += "".join(["&{}={}".format(k, v) for k, v in strmData.items()])
 	strm.write(url)
